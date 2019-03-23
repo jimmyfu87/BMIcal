404b822bdbddce778753a4bf6934e27fdab6687e
jimmyfu87 <jimmyfu87@gmail.com>
2019-03-23 22:15:24 +0800

    init

---
+++ app/src/main/java/com/example/bmical/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.bmical;
+
+import android.support.v7.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
+
+public class MainActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(calcBMI);
+    }
+
+    private View.OnClickListener calcBMI = new View.OnClickListener()
+    {
+        @Override
+        public void onClick(View v) {
+            EditText fieldheight = (EditText) findViewById(R.id.height);
+            EditText fieldweight = (EditText) findViewById(R.id.weight);
+            //身高
+            double height = Double.parseDouble(fieldheight.getText().toString()) / 100;
+            //體重
+            double weight = Double.parseDouble(fieldweight.getText().toString());
+            //計算出BMI值
+            double BMI = weight/ (height * height);
+            String Bstring=String.valueOf(BMI);
+            //結果
+            TextView result = (TextView) findViewById(R.id.result);
+            result.setText(Bstring);
+        }
+    };
+}
